c9b2fd33c9bcfc251c12679bf7eb71f39765feb3
Marius Hagelin <marius.hagelin@gmail.com>
2022-01-10 17:13:21 +0100

    Adding responsivnes

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,54 +11,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 530,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transactions added yet!',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                    height: 350,
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  Text(
+                    'No transactions added yet!',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: constraints.maxHeight * 0.65,
                     child: Image.asset('assets/images/waiting.png',
-                        fit: BoxFit.cover)),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: FittedBox(
-                            child: Text('${transactions[index].amount}kr')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].titel,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id),
+                        fit: BoxFit.cover),
+                  ),
+                ],
+              );
+            },
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: FittedBox(
+                          child: Text('${transactions[index].amount}kr')),
                     ),
                   ),
-                );
+                  title: Text(
+                    transactions[index].titel,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => deleteTx(transactions[index].id),
+                  ),
+                ),
+              );
 
-                /* Card(
+              /* Card(
                   child: Row(
                     children: <Widget>[
                       Container(
@@ -99,9 +102,8 @@ class TransactionList extends StatelessWidget {
                     ],
                   ),
                 ); */
-              },
-              itemCount: transactions.length,
-            ),
-    );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
